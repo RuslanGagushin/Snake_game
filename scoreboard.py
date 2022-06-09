28877072f7dcbd99d838226d786acf2817b62372
RuslanGagushin <gagushin2@gmail.com>
2022-06-09 18:27:09 +0300

    Add high score

--- scoreboard.py
+++ scoreboard.py
@@ -1,14 +1,17 @@
 from turtle import Turtle
 
-ALIGMENT = "center"
+ALIGNMENT = "center"
 FONT = ("Arial", 18, "normal")
+
+with open("HighScore.txt") as file:
+    high_score = file.read()
 
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = int(high_score)
         self.color("cyan")
         self.penup()
         self.goto(0, 270)
@@ -17,11 +20,13 @@ class Scoreboard(Turtle):
 
     def update(self):
         self.clear()
-        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("HighScore.txt", mode="w") as hs:
+                hs.write(str(self.high_score))
         self.score = 0
         self.update()
 
